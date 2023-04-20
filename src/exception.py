db20c204231b9f7a2fbdc7038936884d43ee1224
ankithreddys <ankithreddy653@gmail.com>
2023-04-20 11:47:53 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()  #returns 3 tuples with exception, exception parameter, pin points the line where exception occured
